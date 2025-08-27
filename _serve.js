55a855dade66563f530cc95258e55016482e5a1f
Vinicius Vargas <vinicius.vargas@direction.com.br>
2025-08-27 09:49:39 -0300

    Correção nos MIME types dos arquivos vendor.js, polyfills.js e runtime.js

--- _serve.js
+++ _serve.js
@@ -6,6 +6,7 @@ const port = 3000;
 
 const server = http.createServer((req, res) => {
     console.log(req.method);
+    req.url = decodeURIComponent(req.url);
     if (req.url === '/') {
         const route = path.join(__dirname, 'index.html');
         return returnFile(res, route, 'index.html não encontrado!');
@@ -18,19 +19,22 @@ const server = http.createServer((req, res) => {
 
         return returnFile(res, route, `Asset "${assetName}" não encontrado!`);
     }
-    if (req.url === '/favicon.ico' || req.url.includes('/main') || req.url.includes('/styles')) {
+    if (req.url === '/favicon.ico' || req.url.includes('.js') || req.url.includes('/styles')) {
         const route = path.join(__dirname, req.url);
         let headers;
         if (req.url === '/favicon.ico') headers = { 'Content-Type': 'image/x-icon' };
-        if (req.url.includes('/main')) headers = { 'Content-Type': 'application/javascript' };
+        if (req.url.includes('.js')) headers = { 'Content-Type': 'application/javascript' };
         if (req.url.includes('/styles')) headers = { 'Content-Type': 'text/css' };
 
         res.writeHead(200, headers);
         return returnFile(res, route, `Recurso "${req.url}" não encontrado!`);
     }
 
-    res.writeHead(404);
-    res.end(`Rota "${req.url}" não encontrada`);
+    const route = path.join(__dirname, req.url);
+    returnFile(res, route, `Recurso "${req.url}" não encontrado!`);
+
+    //res.writeHead(404);
+    //res.end(`Rota "${req.url}" não encontrada`);
 });
 
 server.listen(port, () => {
@@ -38,9 +42,9 @@ server.listen(port, () => {
 });
 
 
-function returnFile(res, pathUrl, notFoundMessage) {
-    if (fs.existsSync(pathUrl)) {
-        fs.readFile(pathUrl, (err, data) => {
+function returnFile(res, route, notFoundMessage) {
+    if (fs.existsSync(route)) {
+        fs.readFile(route, (err, data) => {
             if (err) {
                 res.writeHead(500);
                 res.end(`Erro ao ler o arquivo: ${err}`);
